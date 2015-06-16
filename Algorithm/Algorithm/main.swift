321e7d731e3e5f46c43be1ca7608753fc62fd2d5
xiaolovecai <753623217@qq.com>
2015-06-16 22:19:47 +0800

    添加二分查找法

---
+++ Algorithm/Algorithm/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by xiaos on 15/6/16.
+//  Copyright (c) 2015年 com.xsdota. All rights reserved.
+//
+
+import Foundation
+
+//二分查找
+//let myList = [1,2,3,5,7,9,120]
+//println(binarySearch(myList, 120))
+
+
+
